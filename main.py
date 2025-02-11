19c94551c2344b41fbdc116cba4c60565a938274
MisterNoFace <misternofaceofficial@gmail.com>
2025-02-11 22:38:09 +0100

    update

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ with open('data.json') as f:
     data = ast.literal_eval(f.read())
 print(data) 
 current_table={
-"name":'NESSUNA TABELLA APERTA',
+"name":'NO CHART OPENED!',
 "group":[],
 "size":(0,0),
 "table":[],
@@ -35,21 +35,6 @@ def save(sender,app_data):
     dpg.delete_item("new")
     dpg.delete_item("selection")
 
-def pick_random():
-    dpg.delete_item("pick_random")
-    with dpg.window(label="sorteggio casuale",tag="pick_random"):
-        with dpg.group(horizontal=True):
-            dpg.add_text("numero di persone da sorteggiare:")
-            dpg.add_slider_int(width=150,min_value=1,default_value=1,max_value=len(current_table["group"]))
-        dpg.add_text("lista di sorteggio:")
-        with dpg.popup(dpg.last_item()):
-            dpg.add_text("fancy ahh tooltip")
-            """
-            for p in current_table["group"]:
-                with dpg.group(horizontal=True):
-                    dpg.add_text(p)
-                    dpg.add_checkbox(default_value=True)"""
-
 def open_table(sender,app_data,user_data):  
     global current_table,group_list
     current_table=data[user_data]
@@ -72,7 +57,7 @@ def refresh():
 
 def table_configuration():
     dpg.delete_item("table_configuration")
-    with dpg.window(label="configura tabella",tag="table_configuration"):
+    with dpg.window(label="configure chart",tag="table_configuration"):
         with dpg.table(header_row=False, resizable=True, policy=dpg.mvTable_SizingStretchProp,
             borders_outerH=True, borders_innerV=True, borders_outerV=True):
             for i in range(dpg.get_value("table_width")):
@@ -80,30 +65,30 @@ def table_configuration():
             for i in range(dpg.get_value("table_height")):
                 with dpg.table_row():
                     for j in range(dpg.get_value("table_width")):
-                        dpg.add_checkbox(tag=f"{i},{j}")  
+                        dpg.add_checkbox(tag=f"{i},{j}")
 def new_table():
     dpg.delete_item("new")
-    with dpg.window(label="nuovo",tag="new",autosize=True):
+    with dpg.window(label="new chart",tag="new",autosize=True):
         with dpg.group(horizontal=True):
-            dpg.add_text("nome schema:")
+            dpg.add_text("chart name:")
             dpg.add_input_text(tag="table_name")
-        dpg.add_text("aggiungi nomi degli studenti:")
+        dpg.add_text("add here the student's names (separate them with a return):")
         dpg.add_input_text(multiline=True,tag="group")
         with dpg.group(horizontal=True):
-            dpg.add_text("inserisci dimensioni schema:")
+            dpg.add_text("chart dimension (width & height):")
             dpg.add_slider_int(width=50,min_value=3,default_value=10,max_value=20,tag="table_width")
             dpg.add_slider_int(width=50,min_value=3,default_value=10,max_value=20,tag="table_height")
-            dpg.add_button(label="configura tabella",callback=table_configuration)
-        dpg.add_button(label="conferma e salva",callback=save)
+            dpg.add_button(label="configure the chart",callback=table_configuration)
+        dpg.add_button(label="save",callback=save)
 
 def selection():
     dpg.delete_item("selection")
-    with dpg.window(label="seleziona schema",tag="selection",autosize=True):
+    with dpg.window(label="select the chart",tag="selection",autosize=True):
         if len(data)>0:
             for i,item in enumerate(data):
                 with dpg.group(horizontal=True):
                     dpg.add_button(label=item["name"],width=500,callback=open_table,user_data=i)
-                    dpg.add_button(label="elimina",callback=delete_table,user_data=i)
+                    dpg.add_button(label="delete",callback=delete_table,user_data=i)
         else:
             dpg.add_text("nessuno schema presente!")     
 
@@ -112,12 +97,11 @@ def main():
     dpg.delete_item("main")
     with dpg.window(tag="main"):
         with dpg.menu_bar():
-            dpg.add_button(label="apri schema",callback=selection)
-            dpg.add_button(label="nuovo schema",callback=new_table)
-            dpg.add_button(label="sorteggio casuale",callback=pick_random)
+            dpg.add_button(label="open chart",callback=selection)
+            dpg.add_button(label="new chart",callback=new_table)
 
-        dpg.add_text("\nschema: "+current_table["name"])
-        dpg.add_button(label="genera nuovi posti",callback=refresh)
+        dpg.add_text("\nchart: "+current_table["name"])
+        dpg.add_button(label="generate a random seat chart",callback=refresh)
         with dpg.table(header_row=False, resizable=True, policy=dpg.mvTable_SizingStretchProp,borders_outerH=True, borders_innerV=True, borders_outerV=True):
             iter=0
             for i in range(current_table["size"][0]):
@@ -139,7 +123,7 @@ try:
 except:
     pass
 main()
-viewport=dpg.create_viewport(title='generatore posti',width=1200,height=500,resizable=False)
+viewport=dpg.create_viewport(title='random seat chart',width=1200,height=500,resizable=False)
 dpg.setup_dearpygui()
 dpg.show_viewport()
 while dpg.is_dearpygui_running():
